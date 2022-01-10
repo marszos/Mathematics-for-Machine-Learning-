bdac18e0864cc94313ce8babf5538c6d3b90465f
Marcin Szoska <93832363+marszos@users.noreply.github.com>
2022-01-10 10:28:09 -0500

    Create covariance_matrix_by_hand.py

---
+++ covariance_matrix_by_hand.py
@@ -0,0 +1,16 @@
+import numpy as np
+np.random.seed(1)
+N = 10
+b1 = np.random.rand(N)
+b2 = np.random.rand(N)
+X = np.column_stack([b1, b2])
+X -= X.mean(axis=0) 
+fact = N - 1 
+by_hand = np.dot(X.T, X.conj()) / fact
+print(by_hand)
+# [[ 0.04735338  0.01242557]
+#  [ 0.01242557  0.07669083]]
+
+using_cov = np.cov(b1, b2)
+
+assert np.allclose(by_hand, using_cov)
